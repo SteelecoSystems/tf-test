155c13ed8be8de58ef0407d1b254dcba1a315a04
Wheeler Law <wheeler@steele.co>
2023-11-01 18:02:31 -0500

    feat: initial implementation (#1)

---
+++ main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.38"
+    }
+  }
+}
+
+# Configure the GitHub Provider
+provider "github" {
+
+#  app_auth {
+#    id              = "371766"
+#    installation_id = "40392571"
+#    pem_file        = file("~/Downloads/steelecosystems.2023-09-24.private-key.pem")
+#  }
+  owner = "SteelecoSystems"
+}
+
+resource "github_actions_secret" "test_secret" {
+  secret_name = "SECRET_NAME"
+  plaintext_value = "SECRET_${var.secret_name}"
+  repository = "tf-test"
+}
+
+#resource "github_actions_secret" "test_secret_2" {
+#  secret_name = "SECRET_${var.secret_name}"
+#  plaintext_value = "SECRET_${var.secret_name}"
+#  repository = "tf-test"
+#}
+
+variable "secret_name" {
+  description = "The secret name"
+  type = string
+  sensitive = true
+}
