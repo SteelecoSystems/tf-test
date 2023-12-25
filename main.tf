f740d1ed74ebd7640e0324482fbcfb3297255ddd
Wheeler Law <wheeler@steele.co>
2023-12-24 20:24:50 -0600

    chore: test 6 (#6)

--- main.tf
+++ main.tf
@@ -30,12 +30,6 @@ resource "github_actions_variable" "test-variable-2" {
   variable_name = "TEST_VARIABLE_2"
 }
 
-resource "github_actions_variable" "test-variable-3" {
-  repository    = "tf-test"
-  value         = "hello world"
-  variable_name = "TEST_VARIABLE_3"
-}
-
 #resource "github_actions_secret" "test_secret" {
 #  secret_name = "SECRET_NAME"
 #  plaintext_value = "SECRET_${var.secret_name}"
